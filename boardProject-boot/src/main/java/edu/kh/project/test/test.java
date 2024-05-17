ce00ca835fd467a7ba529ce30f1a0af1d7e2fdaf
limbit95@gmail.com <limbit95@gmail.com>
2024-05-17 17:49:16 +0900

    commit

---
+++ boardProject-boot/src/main/java/edu/kh/project/test/test.java
@@ -0,0 +1,34 @@
+package edu.kh.project.test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class test {
+
+//	public static void main(String[] args) {
+//		
+//		try {
+//			FileInputStream file = new FileInputStream(("test.xlsx")); 
+//			Workbook workbook = WorkbookFactory.create(file);
+//			Sheet sheet = workbook.getSheetAt(0);
+//			for(Row row : sheet) {
+//				for(Cell cell : row) {
+//					System.out.println(cell.toString() + "\t");
+//				}
+//				System.out.println();
+//			}
+//			file.close();
+//			
+//		} catch(IOException e) {
+//			e.printStackTrace();
+//		}
+//		
+//	}
+	
+}
