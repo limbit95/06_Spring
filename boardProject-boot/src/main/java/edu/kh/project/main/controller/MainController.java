91cc9e1b225638387386f0fe6aee680c6a048e6f
limbit95@gmail.com <limbit95@gmail.com>
2024-05-24 17:47:21 +0900

    학원

--- boardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
+++ boardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
@@ -1,11 +1,19 @@
 package edu.kh.project.main.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@PropertySource("classpath:/config.properties")
 public class MainController {
+	
+	@Value("${my.public.data.service.key.decode}")
+	private String decodeServiceKey;
 
 	@RequestMapping("/")
 	public String mainPage() {
@@ -19,6 +27,14 @@ public class MainController {
 		ra.addFlashAttribute("message", "로그인 후 이용해주세요");
 		
 		return "redirect:/";
+	}
+	
+	// 공공데이터
+	// 서비스키 리턴하기
+	@ResponseBody
+	@GetMapping("/getServiceKey")
+	public String getServiceKey() {
+		return decodeServiceKey;
 	}
 	
 }
